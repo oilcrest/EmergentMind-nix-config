8fa9280720063dc8acd03eb088bb53a8aa52e251
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-10-24 11:39:30 -0600

    Feat: zsa keymapp: replaces wally-cli and custom zsa-udev module

--- hosts/common/optional/zsa-keeb.nix
+++ hosts/common/optional/zsa-keeb.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   hardware.keyboard.zsa.enable = true;
-  modules.zsa-udev-rules.enable = true; # rules required for flashing zsa keebs https://github.com/zsa/wally/wiki/Linux-install
-  environment.systemPackages = [ pkgs.wally-cli ]; # cli zsa keeb flashing tool
+  environment.systemPackages = [
+    pkgs.keymapp
+  ];
 }
